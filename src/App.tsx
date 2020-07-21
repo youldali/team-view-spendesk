11ca785627d36d71e7e42c57fa6ac231f120da49
Yannis <y.ouldali@yahoo.fr>
2020-07-21 18:13:52 +0200

    Added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Header from 'routes/common/Header';
 import TeamsList from 'routes/teams/TeamsList';
+import UsersList from 'routes/users/UsersList';
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <Header />
-            <TeamsList />
+            <Router>
+                <Header />
+                <Switch>
+                    <Route exact path="/">
+                        <TeamsList />
+                    </Route>
+                    <Route path="/team/:teamId">
+                        <UsersList />
+                    </Route>
+                    <Route path="*">
+                        <Redirect to="/" />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }

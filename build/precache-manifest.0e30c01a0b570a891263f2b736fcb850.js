715a8bbe0d81cd00722c1edf5ce583355edc8937
Yannis <y.ouldali@yahoo.fr>
2020-07-22 17:37:39 +0200

    build

---
+++ build/precache-manifest.0e30c01a0b570a891263f2b736fcb850.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f98b1f22baf3ee979ed393c2bc40f99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1182671b93b3d49fe6b0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4b0347427a75ec65ce09",
+    "url": "/static/js/2.de2b79aa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.de2b79aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1182671b93b3d49fe6b0",
+    "url": "/static/js/main.cf4b61cf.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
